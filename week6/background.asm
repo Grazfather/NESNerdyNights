9db318ce1ee2dd6f1cd009544cebe95b5da5dd7a
Grazfather <grazfather@gmail.com>
2013-09-24 00:15:17 -0700

    week6: Clarify details in comments +cleanup

--- week6/background.asm
+++ week6/background.asm
@@ -21,12 +21,12 @@ RESET:
   STX $2001    ; disable rendering
   STX $4010    ; disable DMC IRQs
 
-vblankwait1:       ; First wait for vblank to make sure PPU is ready
+vblankwait1:   ; First wait for vblank to make sure PPU is ready
   BIT $2002
   BPL vblankwait1
 
 clrmem:
-  LDA #$00
+  LDA #$00     ; memset first 0x800 bytes to 0
   STA $0000, x
   STA $0100, x
   STA $0300, x
@@ -39,7 +39,7 @@ clrmem:
   INX
   BNE clrmem
 
-vblankwait2:      ; Second wait for vblank, PPU is ready after this
+vblankwait2:   ; Second wait for vblank, PPU is ready after this
   BIT $2002
   BPL vblankwait2
 
@@ -47,40 +47,32 @@ vblankwait2:      ; Second wait for vblank, PPU is ready after this
 LoadPalettes:
   LDA $2002             ; read PPU status to reset the high/low latch
   LDA #$3F
-  STA $2006             ; write the high byte of $3F00 address
+  STA $2006             ; write the high byte of $3F00 address (Background palette them sprite palette)
   LDA #$00
   STA $2006             ; write the low byte of $3F00 address
   LDX #$00              ; start out at 0
 LoadPalettesLoop:
   LDA palette, x        ; load data from address (palette + the value in x)
-                          ; 1st time through loop it will load palette+0
-                          ; 2nd time through loop it will load palette+1
-                          ; 3rd time through loop it will load palette+2
-                          ; etc
   STA $2007             ; write to PPU
   INX                   ; X = X + 1
-  CPX #$20              ; Compare X to hex $10, decimal 16 - copying 16 bytes = 4 sprites
+  CPX #$20              ; Compare X to hex $20, decimal 32
   BNE LoadPalettesLoop  ; Branch to LoadPalettesLoop if compare was Not Equal to zero
                         ; if compare was equal to 32, keep going down
-
-
 
 LoadSprites:
   LDX #$00              ; start at 0
 LoadSpritesLoop:
   LDA sprites, x        ; load data from address (sprites +  x)
-  STA $0200, x          ; store into RAM address ($0200 + x)
+  STA $0200, x          ; store into RAM address ($0200 + x) (CPU memory to later be used DMA'd to PPU)
   INX                   ; X = X + 1
   CPX #$10              ; Compare X to hex $10, decimal 16
   BNE LoadSpritesLoop   ; Branch to LoadSpritesLoop if compare was Not Equal to zero
                         ; if compare was equal to 16, keep going down
 
-
-
 LoadBackground:
   LDA $2002             ; read PPU status to reset the high/low latch
   LDA #$20
-  STA $2006             ; write the high byte of $2000 address
+  STA $2006             ; write the high byte of $2000 address (Name Table 0)
   LDA #$00
   STA $2006             ; write the low byte of $2000 address
   LDX #$00              ; start out at 0
@@ -89,14 +81,14 @@ LoadBackgroundLoop:
   STA $2007             ; write to PPU
   INX                   ; X = X + 1
   CPX #$80              ; Compare X to hex $80, decimal 128 - copying 128 bytes
-  BNE LoadBackgroundLoop  ; Branch to LoadBackgroundLoop if compare was Not Equal to zero
+  BNE LoadBackgroundLoop; Branch to LoadBackgroundLoop if compare was Not Equal to zero
                         ; if compare was equal to 128, keep going down
 
 
 LoadAttribute:
   LDA $2002             ; read PPU status to reset the high/low latch
   LDA #$23
-  STA $2006             ; write the high byte of $23C0 address
+  STA $2006             ; write the high byte of $23C0 address (Attribute table 0)
   LDA #$C0
   STA $2006             ; write the low byte of $23C0 address
   LDX #$00              ; start out at 0
@@ -105,11 +97,8 @@ LoadAttributeLoop:
   STA $2007             ; write to PPU
   INX                   ; X = X + 1
   CPX #$08              ; Compare X to hex $08, decimal 8 - copying 8 bytes
-  BNE LoadAttributeLoop  ; Branch to LoadAttributeLoop if compare was Not Equal to zero
+  BNE LoadAttributeLoop ; Branch to LoadAttributeLoop if compare was Not Equal to zero
                         ; if compare was equal to 128, keep going down
-
-
-
 
 
   LDA #%10010000   ; enable NMI, sprites from Pattern Table 0, background from Pattern Table 1
@@ -119,12 +108,12 @@ LoadAttributeLoop:
   STA $2001
 
 Forever:
-  JMP Forever     ;jump back to Forever, infinite loop
+  JMP Forever     ; jump back to Forever, infinite loop
 
 
 
 NMI:
-  LDA #$00
+  LDA #$00        ; Set up DMA for sprite data
   STA $2003       ; set the low byte (00) of the RAM address
   LDA #$02
   STA $4014       ; set the high byte (02) of the RAM address, start the transfer
@@ -134,7 +123,7 @@ LatchController:
   LDA #$01
   STA $4016
   LDA #$00
-  STA $4016       ; tell both the controllers to latch buttons
+  STA $4016       ; tell both controllers to latch buttons
 
 
 ReadA:
