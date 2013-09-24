297a764e62a7af4e2aab9ed0013ba589beaacd76
Grazfather <grazfather@gmail.com>
2013-09-23 23:24:26 -0700

    week5: Move all sprites

--- week5/controller.asm
+++ week5/controller.asm
@@ -111,25 +111,41 @@ LatchController:
 ReadLeft:
   LDA $4016       ; player 1 - Left
   AND #%00000001  ; only look at bit 0
-  BEQ ReadLeftDone   ; branch to ReadADone if button is NOT pressed (0)
+  BEQ ReadLeftDone; branch to ReadADone if button is NOT pressed (0)
                   ; add instructions here to do something when button IS pressed (1)
-  LDA $0203       ; load sprite X position
+  LDX #$00        ; x = 0
+MoveSpriteLeft:
+  LDA $0203,x     ; load sprite X position
   SEC             ; make sure the carry flag is set
   SBC #$01        ; A = A - 1
-  STA $0203       ; save sprite X position
-ReadLeftDone:        ; handling this button is done
+  STA $0203,x     ; save sprite X position
+  TXA             ; A = X
+  CLC             ; make sure carry flag is clear
+  ADC #$04        ; A +=4
+  TAX             ; X = A
+  CPX #$10        ; 4 sprites: 16 bytes
+  BNE MoveSpriteLeft
+ReadLeftDone:     ; handling this button is done
 
 
 ReadRight:
-  LDA $4016       ; player 1 - Right
+  LDA $4016       ; player 1 - A
   AND #%00000001  ; only look at bit 0
-  BEQ ReadRightDone   ; branch to ReadRightDone if button is NOT pressed (0)
+  BEQ ReadRightDone; branch to ReadADone if button is NOT pressed (0)
                   ; add instructions here to do something when button IS pressed (1)
-  LDA $0203       ; load sprite X position
-  CLC             ; make sure carry flag is clear
+  LDX #$00        ; x = 0
+MoveSpriteRight:
+  LDA $0203,x     ; load sprite X position
+  CLC             ; make sure the carry flag is clear
   ADC #$01        ; A = A + 1
-  STA $0203       ; save sprite X position
-ReadRightDone:        ; handling this button is done
+  STA $0203,x     ; save sprite X position
+  CLC             ; make sure carry flag is clear
+  TXA             ; A = X
+  ADC #$04        ; A += 4
+  TAX             ; X = A -> Net effect: X += 4
+  CPX #$10        ; 4 sprites: 16 bytes
+  BNE MoveSpriteRight
+ReadRightDone:    ; handling this button is done
 
 
 
