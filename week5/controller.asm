03806321cbea01e02615e56b5504ec017f324e16
Grazfather <grazfather@gmail.com>
2013-09-23 23:13:59 -0700

    week5: Use left and right keys instead of a/b

--- week5/controller.asm
+++ week5/controller.asm
@@ -101,38 +101,44 @@ LatchController:
   LDA #$00
   STA $4016       ; tell both the controllers to latch buttons
 
-
-ReadA: 
   LDA $4016       ; player 1 - A
-  AND #%00000001  ; only look at bit 0
-  BEQ ReadADone   ; branch to ReadADone if button is NOT pressed (0)
-                  ; add instructions here to do something when button IS pressed (1)
-  LDA $0203       ; load sprite X position
-  CLC             ; make sure the carry flag is clear
-  ADC #$01        ; A = A + 1
-  STA $0203       ; save sprite X position
-ReadADone:        ; handling this button is done
-  
-
-ReadB: 
   LDA $4016       ; player 1 - B
+  LDA $4016       ; player 1 - Select
+  LDA $4016       ; player 1 - Start
+  LDA $4016       ; player 1 - Up
+  LDA $4016       ; player 1 - Down
+
+ReadLeft:
+  LDA $4016       ; player 1 - Left
   AND #%00000001  ; only look at bit 0
-  BEQ ReadBDone   ; branch to ReadBDone if button is NOT pressed (0)
+  BEQ ReadLeftDone   ; branch to ReadADone if button is NOT pressed (0)
                   ; add instructions here to do something when button IS pressed (1)
   LDA $0203       ; load sprite X position
-  SEC             ; make sure carry flag is set
+  SEC             ; make sure the carry flag is set
   SBC #$01        ; A = A - 1
   STA $0203       ; save sprite X position
-ReadBDone:        ; handling this button is done
+ReadLeftDone:        ; handling this button is done
 
 
-  
+ReadRight:
+  LDA $4016       ; player 1 - Right
+  AND #%00000001  ; only look at bit 0
+  BEQ ReadRightDone   ; branch to ReadRightDone if button is NOT pressed (0)
+                  ; add instructions here to do something when button IS pressed (1)
+  LDA $0203       ; load sprite X position
+  CLC             ; make sure carry flag is clear
+  ADC #$01        ; A = A + 1
+  STA $0203       ; save sprite X position
+ReadRightDone:        ; handling this button is done
+
+
+
   RTI             ; return from interrupt
- 
-;;;;;;;;;;;;;;  
-  
-  
-  
+
+;;;;;;;;;;;;;;
+
+
+
   .bank 1
   .org $E000
 palette:
