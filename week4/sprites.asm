342127c7ca4ff3115a9536a1dea17136c0c3c320
Grazfather <grazfather@gmail.com>
2014-03-24 20:07:50 -0700

    week4: Remove trailing whitespace

--- week4/sprites.asm
+++ week4/sprites.asm
@@ -2,13 +2,13 @@
   .ineschr 1   ; 1x  8KB CHR data
   .inesmap 0   ; mapper 0 = NROM, no bank swapping
   .inesmir 1   ; background mirroring
-  
+
 
 ;;;;;;;;;;;;;;;
 
-    
+
   .bank 0
-  .org $C000 
+  .org $C000
 RESET:
   SEI          ; disable IRQs
   CLD          ; disable decimal mode
@@ -38,7 +38,7 @@ clrmem:
   STA $0200, x    ;move all sprites off screen
   INX
   BNE clrmem
-   
+
 vblankwait2:      ; Second wait for vblank, PPU is ready after this
   BIT $2002
   BPL vblankwait2
@@ -57,7 +57,7 @@ LoadPalettesLoop:
   LDA palette, x        ;load palette byte
   STA $2007             ;write to PPU
   INX                   ;set index to next byte
-  CPX #$20            
+  CPX #$20
   BNE LoadPalettesLoop  ;if x = $20, 32 bytes copied, all done
 
 
@@ -65,7 +65,7 @@ LoadPalettesLoop:
   LDA #$80
   STA $0200        ; put sprite 0 in center ($80) of screen vert
   STA $0203        ; put sprite 0 in center ($80) of screen horiz
-  LDA #$00
+  LDA #$01
   STA $0201        ; tile number = 0
   STA $0202        ; color = 0, no flipping
 
@@ -77,21 +77,21 @@ LoadPalettesLoop:
 
 Forever:
   JMP Forever     ;jump back to Forever, infinite loop
-  
- 
+
+
 
 NMI:
   LDA #$00
   STA $2003  ; set the low byte (00) of the RAM address
   LDA #$02
   STA $4014  ; set the high byte (02) of the RAM address, start the transfer
-  
+
   RTI        ; return from interrupt
- 
-;;;;;;;;;;;;;;  
-  
-  
-  
+
+;;;;;;;;;;;;;;
+
+
+
   .bank 1
   .org $E000
 palette:
@@ -100,16 +100,16 @@ palette:
 
 
   .org $FFFA     ;first of the three vectors starts here
-  .dw NMI        ;when an NMI happens (once per frame if enabled) the 
+  .dw NMI        ;when an NMI happens (once per frame if enabled) the
                    ;processor will jump to the label NMI:
   .dw RESET      ;when the processor first turns on or is reset, it will jump
                    ;to the label RESET:
   .dw 0          ;external interrupt IRQ is not used in this tutorial
-  
-  
-;;;;;;;;;;;;;;  
-  
-  
+
+
+;;;;;;;;;;;;;;
+
+
   .bank 2
   .org $0000
   .incbin "mario.chr"   ;includes 8KB graphics file from SMB1
